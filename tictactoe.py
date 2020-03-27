5dd31f797705e5def794023b05ed239a81fed197
Andrew Kicheev <kicheevandrey@gmail.com>
2020-03-27 12:35:40 +0300

    Field borders generation and game state input

--- tictactoe.py
+++ tictactoe.py
@@ -11,21 +11,58 @@ def random_fill():
         line_fill[rand] = 'X'
         set_cells.remove(rand)
 
-    fill_field = []
-    while line_fill:
-        fill_field.append(line_fill[:3])
-        line_fill = line_fill[3:]
-
-    return fill_field
+    return line_to_matrix(line_fill)
 
 
+def line_to_matrix(line):
+
+    if len(line) < 9:
+        line = line + ' ' * (9 - len(line))
+
+    matrix_dim = 3
+    matrix = [['' for j in range(matrix_dim)] for i in range(matrix_dim)]
+    for i in range(matrix_dim):
+        for j in range(matrix_dim):
+            matrix[i][j] = line[j]
+        line = line[matrix_dim:]
+    return matrix
+
+
+def matrix_to_line(matrix):
+    line = [j for i in matrix for j in i]
+    return line
+
+
+def add_borders(matrix):
+    ud_borders = '-'
+    lr_borders = '|'
+
+    field_height = 5
+    field_lenght = 9
+    field = [[' ' for j in range(field_lenght)] for i in range(field_height)]
+
+    line = matrix_to_line(matrix)
+    line_index = 0
+
+    for i in range(field_height):
+        for j in range(field_lenght):
+            if i == 0 or i == field_height - 1:
+                field[i][j] = ud_borders
+            elif j == 0 or j == field_lenght - 1:
+                field[i][j] = lr_borders
+            elif j % 2 == 0:
+                field[i][j] = line[line_index]
+                line_index += 1
+
+    return field
+
+
+test_str = 'O_OXXO_XX'
 marks = ('X', 'O')
-dim = 3
-field = [[0 for j in range(dim)]for i in range(dim)]
 
-field = random_fill()
+matrix = line_to_matrix(input('Enter cells: '))
+# matrix = line_to_matrix(test_str)
+field = add_borders(matrix)
 
-for i in range(dim):
-    for j in range(dim):
-        print(field[i][j], end=' ')
-    print()
+for i in range(len(field)):
+    print(*field[i], sep='')
