d8b6092a73d73e1101dd3d0d4ef05507295f51bc
Andrew Kicheev <kicheevandrey@gmail.com>
2020-04-02 02:12:45 +0300

    Removed useless functions. Fix «check_state» function. Function «add_borders» modified to function «print_matrix_with_borders». Add scalability. Add moves.

--- tictactoe.py
+++ tictactoe.py
@@ -1,30 +1,16 @@
 import random
 
 
-def random_fill():
-    n = 9
-    set_cells = [i for i in range(n)]
-    line_fill = ['O' for i in range(n)]
+def line_to_matrix(line, dimension=3):
 
-    for i in range(n // 2):
-        rand = random.choice(set_cells)
-        line_fill[rand] = 'X'
-        set_cells.remove(rand)
+    if len(line) < dimension ** 2:
+        line = line + ' ' * (dimension ** 2 - len(line))
 
-    return line_to_matrix(line_fill)
-
-
-def line_to_matrix(line):
-
-    if len(line) < 9:
-        line = line + ' ' * (9 - len(line))
-
-    matrix_dim = 3
-    matrix = [['' for j in range(matrix_dim)] for i in range(matrix_dim)]
-    for i in range(matrix_dim):
-        for j in range(matrix_dim):
+    matrix = [['' for j in range(dimension)] for i in range(dimension)]
+    for i in range(dimension):
+        for j in range(dimension):
             matrix[i][j] = line[j]
-        line = line[matrix_dim:]
+        line = line[dimension:]
     return matrix
 
 
@@ -35,84 +21,75 @@ def print_matrix(matrix):
         print()
 
 
-def matrix_to_line(matrix):
-    line = [j for i in matrix for j in i]
-    return line
+def print_matrix_with_borders(matrix, dimension=3):
 
-
-def add_borders(matrix):
     ud_borders = '-'
     lr_borders = '|'
 
-    field_height = 5
-    field_lenght = 9
-    field = [[' ' for j in range(field_lenght)] for i in range(field_height)]
+    border_lenght = dimension + 2
 
-    line = matrix_to_line(matrix)
-    line_index = 0
-
-    for i in range(field_height):
-        for j in range(field_lenght):
-            if i == 0 or i == field_height - 1:
-                field[i][j] = ud_borders
-            elif j == 0 or j == field_lenght - 1:
-                field[i][j] = lr_borders
-            elif j % 2 == 0:
-                field[i][j] = line[line_index]
-                line_index += 1
-
-    return field
+    print(ud_borders * (border_lenght * 2 - 1))
+    for i in range(dimension):
+        for j in range(border_lenght):
+            if j == 0:
+                print(lr_borders, end=' ')
+            elif j == border_lenght - 1:
+                print(lr_borders)
+            else:
+                print(matrix[i][j-1], end=' ')
+    print(ud_borders * (border_lenght * 2 - 1))
 
 
-def check_state(line):
-    Xs = line.count('X')
-    Os = line.count('O')
-    Ss = line.count('_')
+def check_state(matrix):
 
     win_cases = []
 
-    for i in range(3):
-        temp_list = []
-        for j in range(i, i + 7, 3):
-            temp_list.append(line[j])
-        win_cases.append(''.join(temp_list))
+    # Count marks
+    Xs, Os, Ss = 0, 0, 0
+    for i in matrix:
+        for j in i:
+            Xs += 1 if j == 'X' else 0
+            Os += 1 if j == 'O' else 0
+            Ss += 1 if j == ' ' else 0
 
-    for i in range(0, 9, 3):
-        win_cases.append(line[i:i + 3])
+    # Horizontals
+    for i in matrix:
+        win_cases.append(i)
 
-    temp_list = []
-    for i in range(0, 9, 4):
-        temp_list.append(line[i])
-    win_cases.append(''.join(temp_list))
+    # Verticals
+    for j in range(len(matrix)):
+        win_cases.append([matrix[i][j] for i in range(len(matrix))])
 
-    temp_list = []
-    for i in range(2, 7, 2):
-        temp_list.append(line[i])
-    win_cases.append(''.join(temp_list))
+    # Diagonals
+    win_cases.append([matrix[i][i] for i in range(len(matrix))])
+    win_cases.append([matrix[i][-(i+1)] for i in range(len(matrix))])
 
+    # Win cases counter
     win_cases_counter = 0
     win_mark = ''
-
-    for i in range(8):
-        if len(set(win_cases[i])) == 1:
+    for i in win_cases:
+        if len(set(i)) == 1 and i[0] != ' ':
             win_cases_counter += 1
-            win_mark = win_cases[i][0]
+            win_mark = i[0]
 
+    # Check state
     if abs(Xs - Os) > 1:
         print('Impossible')
     elif win_cases_counter > 1:
         print('Impossible')
     elif win_cases_counter == 1:
         print(win_mark, 'wins')
+        return True
     elif Ss == 0:
         print('Draw')
-    else:
-        print('Game not finished')
+        return True
+    # else:
+    #     print('Game not finished')
 
-    return
+    return False
 
 
-def input_coordinates():
+def input_coordinates(mark, dimension=3):
 
     global matrix
 
@@ -127,37 +104,30 @@ def input_coordinates():
         x = int(input_string[0])
         y = int(input_string[1])
 
-        if not ((1 <= x <= 3) and (1 <= y <= 3)):
+        if not ((1 <= x <= dimension) and (1 <= y <= dimension)):
             print('Coordinates should be from 1 to 3!')
             continue
 
         if matrix[-y][x-1] != ' ':
             print('This cell is occupied! Choose another one!')
-            print(matrix[-y][x-1])
             continue
 
         else:
-            matrix[-y][x-1] = 'X'
+            matrix[-y][x-1] = mark
             break
 
 
-input_list = ['X_X_O____', '_XXOO_OX_', '789456123']
+# list_of_states = ['XXXOO__O_', 'XOXOXOXXO', 'XOOOXOXXO', 'XOXOOXXXO', 'XO_OOX_X_', 'XO_XO_XOX', '_O_X__X_X', '_OOOO_X_X']
+# matrix = line_to_matrix(list_of_states[5])
+matrix = line_to_matrix('')
+marks = (' ', 'X', 'O')
+mark_index = 1
 
-input_string = input('Enter cells: ').replace('_', ' ')
-# input_string = input_list[1].replace('_', ' ')
+print_matrix_with_borders(matrix)
 
-matrix = line_to_matrix(input_string)
+while not check_state(matrix):
 
-field = add_borders(matrix)
-for i in field:
-    print(*i, sep='')
+    input_coordinates(marks[mark_index])
+    mark_index = -mark_index
 
-input_coordinates()
-
-field = add_borders(matrix)
-for i in field:
-    print(*i, sep='')
-
-# check_state(input_string)
-
-
+    print_matrix_with_borders(matrix)
